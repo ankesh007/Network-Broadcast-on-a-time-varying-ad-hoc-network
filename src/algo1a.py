f54d085940e84741640d793ae7d836fdeb3bffba
MukundMundhra <mukund8102@gmail.com>
2017-10-25 23:38:00 +0530

    Script for part 1 added

---
+++ src/algo1a.py
@@ -0,0 +1,142 @@
+from __future__ import division
+import csv
+import sys
+
+filename="../proximityedgestimestamps.csv"
+
+
+class DATA:
+	def __init__(self,id):
+		self.id=id
+
+class DEVICE:
+
+	def __init__(self,id):
+		self.id=id
+		self.to_forward={}
+		self.received={}
+
+	def has_data(self,data):
+		if(data.id in self.received):
+			return True
+		if(data.id in self.to_forward):
+			return True
+		return False
+
+	def receive_data(self,data,device):
+		self.received[data.id]=(data,device)
+		self.to_forward[data.id]=(data,0)
+
+	def update_forwarding(self,K):
+		temp_list=[]
+		for chunk in self.to_forward:
+			chunks=self.to_forward[chunk]
+			if(chunks[1]==K):
+				temp_list.append(chunk)
+		for chunk_id in temp_list:
+			del self.to_forward[chunk_id]
+
+
+def main():
+
+	if(len(sys.argv)<3):
+		print("Use: <script_name> <Parameter_K> <Source_node>")
+		exit(0)
+
+	devices={}
+	brodcasted_to=0
+
+	K=int(sys.argv[1])
+	data_generated_at=int(sys.argv[2])
+	percent_check=0.9
+	start_time=0
+
+	device_count=1
+	with open(filename,'rb') as csvfile:
+		trace=csv.reader(csvfile,delimiter=';')
+
+		for row in trace:
+			if(len(row)<3):
+				continue
+			device_id1=int(row[1])
+			device_id2=int(row[2])
+
+			if(device_id1 in devices):
+				pass
+			else:
+				devices[device_id1]=DEVICE(device_id1)
+				device_count=device_count+1
+
+			if(device_id2 in devices):
+				pass
+			else:
+				devices[device_id2]=DEVICE(device_id2)
+				device_count=device_count+1
+	
+
+	data_id=str(data_generated_at)+"_0" 
+	#convention that data_id for generated data is <device_id>_<brodcast_count>
+	data=DATA(data_id)
+	device_with_data=DEVICE(data_generated_at)
+	device_with_data.to_forward[data_id]=(data,0)
+	devices[data_generated_at]=device_with_data
+	devices[data_generated_at].update_forwarding(K)
+	flag=False
+
+
+	with open(filename,'rb') as csvfile:
+		trace=csv.reader(csvfile,delimiter=';')
+
+		for row in trace:
+			if(len(row)<3):
+				continue
+			
+			time_stamp=int(row[0])
+			device_id1=int(row[1])
+			device_id2=int(row[2])
+			for chunk in devices[device_id1].to_forward:
+				chunks=devices[device_id1].to_forward[chunk]
+
+				if(devices[device_id2].has_data(chunks[0])==False):
+					devices[device_id2].receive_data(chunks[0],devices[device_id1])
+					brodcasted_to=brodcasted_to+1
+					devices[device_id1].to_forward[chunks[0].id]=(chunks[0],chunks[1]+1)
+					# print brodcasted_to," ",device_id1," ",device_id2 
+					if(brodcasted_to>=device_count*percent_check):
+						# print K,",""Time:",time_stamp
+						print "1",time_stamp
+						flag=True
+						break
+
+			if(flag==True):
+				break
+			devices[device_id1].update_forwarding(K)
+
+			for chunk in devices[device_id2].to_forward:
+				chunks=devices[device_id2].to_forward[chunk]
+
+				if(devices[device_id1].has_data(chunks[0])==False):
+					devices[device_id1].receive_data(chunks[0],devices[device_id2])
+					brodcasted_to=brodcasted_to+1
+					devices[device_id2].to_forward[chunks[0].id]=(chunks[0],chunks[1]+1)
+					# print brodcasted_to," ",device_id2," ",device_id1
+					if(brodcasted_to>=device_count*percent_check):
+						# print K,",""Time:",time_stamp
+						print "1",time_stamp
+						flag=True
+						break
+			
+			if(flag==True):
+				break
+			devices[device_id2].update_forwarding(K)
+
+		if(flag==False):
+			# print K,",","Reached only(in %):",(brodcasted_to/device_count)*100
+			print "0",(brodcasted_to/device_count)*100
+
+
+if __name__=="__main__":
+	main()
+
+
+
---
+++ src/script1.py
@@ -0,0 +1,82 @@
+import subprocess
+import random
+import csv
+import numpy as np
+
+
+# For starting device as 26, running for k = 0 to 100
+
+file_percent = open("26_percent.csv",'w')
+file_time = open("26_time.csv",'w')
+
+for j in range(1,100):		# set range(100)
+	x = subprocess.check_output(["python", "algo1a.py", str(j), str(26) ])
+	# print x
+	y = x.split()
+	# print y
+	if int(y[0])==1 :
+		file_time.write(str(j) + ',' + y[1] + '\n')
+	else:
+		file_percent.write(str(j) + ',' + y[1] + '\n')
+
+file_percent.close()
+file_time.close()
+# done 1a
+
+# For random sarting points
+
+devices = set()
+filename="../proximityedgestimestamps.csv"
+
+with open(filename,'rb') as csvfile:
+	trace=csv.reader(csvfile,delimiter=';')
+
+	for row in trace:
+		if(len(row)<3):
+			continue
+
+		device_id1=int(row[1])
+		device_id2=int(row[2])
+		devices.add(device_id1)
+		devices.add(device_id2)
+
+total = len(devices)
+device_list = list(devices)
+
+file_percent = open("avg_percent.csv",'w')
+file_time = open("avg_time.csv",'w')
+
+start_nodes = []
+
+for j in range(100):
+	x = random.randint(0,total-1)
+	start_nodes.append(x)
+
+for k in range(1,100):
+
+	# count_percent = 0
+	# count_time = 0
+	# total_percent = 0
+	# total_time = 0
+
+	times = []
+	percents = []
+
+	for j in range(100):		# set range(100)
+		x = subprocess.check_output(["python", "algo1a.py", str(k), str(start_nodes[j]) ])
+		# print x
+		y = x.split()
+		# print y
+		if int(y[0])==1 :
+			# count_time+=1
+			# total_time+=float(y[1])
+			times.append(float(y[1]))
+		else:
+			percents.append(float(y[1]))
+
+	file_percent.write(str(k) + ',' + str(np.mean(percents)) + ',' + str(np.std(percents)) + '\n')
+	file_time.write(str(k) + ',' + str(np.mean(times)) + ',' + str(np.std(times)) + '\n')
+
+
+file_percent.close()
+file_time.close()	
