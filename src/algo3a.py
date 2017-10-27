3fc236b219f74528bb553766f9f638cfe38b607c
MukundMundhra <mukund8102@gmail.com>
2017-10-27 23:15:58 +0530

    Ginny added to Script2

--- src/algo3a.py
+++ src/algo3a.py
@@ -6,6 +6,7 @@ import random
 import numpy as np
 
 filename="../proximityedgestimestamps.csv"
+filename2="../modularityclass.csv"
 
 
 class DATA:
@@ -42,27 +43,41 @@ class DEVICE:
 
 def main():
 
-	if(len(sys.argv)<6):
-		print("Use: <script_name> <Top_S%> <Bottom_L%> <Transmission_Prob_to_super_nodes(%)> <Transmission_Prob_to_Oridinary_nodes(%)> <Source_Node>")
+	if(len(sys.argv)<4):
+		print("Use: <script_name> <Transmission Prob.(X in %)> <Transmission Porb(Y in %)> <Source_Node>")
 		exit(0)
 
 	devices={}
-	device_neighbour_set={}
+	device_community={}
 	device_broadcast_count={}
 	brodcasted_to=0
 
-	S=float(sys.argv[1])/100
-	L=float(sys.argv[2])/100
-	TP=float(sys.argv[3])
-	TPO=float(sys.argv[4])
+	# K=int(sys.argv[1])
+	X=float(sys.argv[1])
+	Y=float(sys.argv[2])
 
 	percent_check=0.9
 
-	data_generated_at=int(sys.argv[5])
+	data_generated_at=int(sys.argv[3])
 	start_time=0
 	device_count=0
 
 # *************Processing the trace file for classifying hosts according to degree******************
+
+	with open(filename2,'rb') as csvfile:
+		trace=csv.reader(csvfile,delimiter=';')
+
+		for row in trace:
+			if(len(row)<2):
+				continue
+
+			device_id1=int(row[0])
+			class_device=int(row[1])
+
+			if(device_id1 in device_community):
+				pass
+			else:
+				device_community[device_id1]=class_device
 
 	with open(filename,'rb') as csvfile:
 		trace=csv.reader(csvfile,delimiter=';')
@@ -74,69 +89,24 @@ def main():
 			device_id2=int(row[2])
 
 			if(device_id1 in devices):
-				device_neighbour_set[device_id1].add(device_id2)
 				pass
 			else:
 				devices[device_id1]=DEVICE(device_id1)
 				device_count=device_count+1
 				device_broadcast_count[device_id1]=0
-				device_neighbour_set[device_id1]=set()
-				device_neighbour_set[device_id1].add(device_id2)
+
+				# print device_community[device_id1]
 
 			if(device_id2 in devices):
-				device_neighbour_set[device_id2].add(device_id1)
 				pass
 			else:
 				devices[device_id2]=DEVICE(device_id2)
 				device_count=device_count+1
 				device_broadcast_count[device_id2]=0
-				device_neighbour_set[device_id2]=set()
-				device_neighbour_set[device_id2].add(device_id1)
 
-# ***************File Reading Ended**************************
+				# print device_community[device_id2]
+
 	
-	device_degree_count={}
-
-# ****************Extracting length of neighbour set************************
-	for elements in device_neighbour_set:
-		device_degree_count[elements]=len(device_neighbour_set[elements])
-
-# ****************Sorting dictionary according to number of unique neighbours and storing in list***********
-	sorted_device_degree = sorted(device_degree_count.items(), key=operator.itemgetter(1))
-	device_category={}
-
-	super_node_count=int(S*device_count)
-	weakly_connected_node_count=int(L*device_count)
-
-# Assigning Super nodes Category 1 in a dictionary
-
-	temp_var=device_count-1
-	while True:
-		device_category[sorted_device_degree[temp_var][0]]=1
-		temp_var=temp_var-1
-		if(temp_var<device_count-super_node_count):
-			break
-
-# Assigning Weak nodes Category 3 in a dictionary
-
-	temp_var=0
-	while True:
-		device_category[sorted_device_degree[temp_var][0]]=3
-		temp_var=temp_var+1
-		if(temp_var>weakly_connected_node_count):
-			break
-
-# Assigning Middle Class Ordinary nodes category 2 in a dictionary
-
-	for elements in sorted_device_degree:
-		if(elements[0] in device_category):
-			pass
-		else:
-			device_category[elements[0]]=2
-
-# *********************************************************************
-
-	#convention that data_id for generated data is <device_id>_<brodcast_count>
 	device_with_data=devices[data_generated_at]
 	data_id=str(data_generated_at)+"_0" 
 	data=DATA(data_id)
@@ -144,7 +114,6 @@ def main():
 	devices[data_generated_at]=device_with_data
 	flag=False
 
-	transmission_list=[0,0,0]
 
 	with open(filename,'rb') as csvfile:
 		trace=csv.reader(csvfile,delimiter=';')
@@ -163,19 +132,18 @@ def main():
 
 				rand_int=random.randint(0,101)
 
-				if(device_category[device_id2]==1 and rand_int>TP):
+				if(device_community[device_id1]==device_community[device_id2] and rand_int>Y):
 					continue
-				if(device_category[device_id2]==2 and rand_int>TPO):
-					continue
+				if(device_community[device_id1]!=device_community[device_id2] and rand_int>X):
+					continue				
 				if(devices[device_id2].has_data(chunks[0])==False):
 					devices[device_id2].receive_data(chunks[0],devices[device_id1])
 					brodcasted_to=brodcasted_to+1
 					devices[device_id1].to_forward[chunks[0].id]=(chunks[0],chunks[1]+1)
-					transmission_list[device_category[device_id1]-1]=transmission_list[device_category[device_id1]-1]+1
 					device_broadcast_count[device_id1]=device_broadcast_count[device_id1]+1
-					# print brodcasted_to," ",device_id1," ",device_id2 
+
 					if(brodcasted_to>=device_count*percent_check):
-						# print "Time:",time_stamp
+						# print "X:",X," Y:",Y," Time:",time_stamp
 						print "1",time_stamp
 						flag=True
 						break
@@ -186,22 +154,20 @@ def main():
 #Device 2 transmission
 			for chunk in devices[device_id2].to_forward:
 				chunks=devices[device_id2].to_forward[chunk]
-				rand_int=random.randint(0,101)				
-				if(device_category[device_id1]==1 and rand_int>TP):
-					continue
-				if(device_category[device_id1]==2 and rand_int>TPO):
-					continue				
+				rand_int=random.randint(0,101)
 
+				if(device_community[device_id1]==device_community[device_id2] and rand_int>Y):
+					continue
+				if(device_community[device_id1]!=device_community[device_id2] and rand_int>X):
+					continue				
 				if(devices[device_id1].has_data(chunks[0])==False):
 					devices[device_id1].receive_data(chunks[0],devices[device_id2])
 					brodcasted_to=brodcasted_to+1
 					devices[device_id2].to_forward[chunks[0].id]=(chunks[0],chunks[1]+1)
-					transmission_list[device_category[device_id2]-1]=transmission_list[device_category[device_id2]-1]+1
 					device_broadcast_count[device_id2]=device_broadcast_count[device_id2]+1
 
-				# print brodcasted_to," ",device_id2," ",device_id1
 					if(brodcasted_to>=device_count*percent_check):
-						# print "Time:",time_stamp
+						# print "X:",X," Y:",Y," Time:",time_stamp
 						print "1",time_stamp
 						flag=True
 						break
@@ -210,14 +176,8 @@ def main():
 				break
 
 		if(flag==False):
-			# print "Reached only(in %):",(brodcasted_to/device_count)*100
+			# print "X:",X," Y:",Y,",","Reached only(in %):",(brodcasted_to/device_count)*100
 			print "0",(brodcasted_to/device_count)*100
-
-		transmission_list[0]=transmission_list[0]/super_node_count
-		transmission_list[2]=transmission_list[2]/weakly_connected_node_count
-		transmission_list[1]=transmission_list[1]/(device_count-super_node_count-weakly_connected_node_count)
-		# print "TransmissionsCount by [Super,Ordinary,Weak]:",transmission_list
-		print transmission_list[0],transmission_list[1],transmission_list[2]
 
 	device_broacast_list=[]
 
--- src/script2.py
+++ src/script2.py
@@ -6,11 +6,12 @@ import numpy as np
 
 # For starting device as 26, running for s = 0.5, l = 70 , x = 1 to 99
 
-file_percent = open("26_percent.csv",'w')
-file_time = open("26_time.csv",'w')
-file_trans = open("26_trans.csv",'w')
+file_percent = open("part2/26_percent.csv",'w')
+file_time = open("part2/26_time.csv",'w')
+file_trans = open("part2/26_trans.csv",'w')
+file_ginny = open("part2/26_ginny.csv",'w')
 
-for x in range(1,100):		# set range(1,100)
+for x in range(0,101):		# set range(0,101)
 	z = subprocess.check_output(["python", "algo2a.py", str(0.5), str(70), str(x), str(100-x), str(26) ])
 	# print x
 	y = z.split()
@@ -21,10 +22,12 @@ for x in range(1,100):		# set range(1,100)
 		file_percent.write(str(x) + ',' + y[1] + '\n')
 
 	file_trans.write(str(x) + ',' + y[2] + ',' + y[3] + ',' + y[4] + '\n')
+	file_ginny.write(str(x) + ',' + y[5] + '\n')
 
 file_percent.close()
 file_time.close()
 file_trans.close()
+file_ginny.close()
 # done 2a
 
 # For random sarting points
@@ -47,9 +50,10 @@ with open(filename,'rb') as csvfile:
 total = len(devices)
 device_list = list(devices)
 
-file_percent = open("avg_percent.csv",'w')
-file_time = open("avg_time.csv",'w')
-file_trans = open("avg_trans.csv",'w')
+file_percent = open("part2/avg_percent.csv",'w')
+file_time = open("part2/avg_time.csv",'w')
+file_trans = open("part2/avg_trans.csv",'w')
+file_ginny = open("part2/avg_ginny.csv",'w')
 
 start_nodes = []
 
@@ -69,6 +73,7 @@ for x in range(1,100):		# set range(1,100)
 	trans_super = []
 	trans_ord = []
 	trans_weak = []
+	ginny = []
 
 	for j in range(100):		# set range(100)
 		z = subprocess.check_output(["python", "algo2a.py", str(0.5), str(70), str(x), str(100-x), str(start_nodes[j]) ])
@@ -80,21 +85,39 @@ for x in range(1,100):		# set range(1,100)
 		trans_super.append(float(y[2]))
 		trans_ord.append(float(y[3]))
 		trans_weak.append(float(y[4]))
+		ginny.append(float(y[5]))
 
-	file_percent.write(str(x) + ',' + str(np.mean(percents)) + ',' + str(np.std(percents)) + '\n')
-	file_time.write(str(x) + ',' + str(np.mean(times)) + ',' + str(np.std(times)) + '\n')
+	mnp = 0
+	stdp = 0
+	if len(percents)==0:
+		mnp = 0
+	else:
+		mnp = np.mean(percents)
+		stdp = np.std(percents)
+	mnt = 0
+	stdt = 0
+	if len(times)==0:
+		mnt = 0
+	else:
+		mnt = np.mean(times)
+		stdt = np.std(times)
+
+	file_percent.write(str(x) + ',' + str(mnp) + ',' + str(stdp) + '\n')
+	file_time.write(str(x) + ',' + str(mnt) + ',' + str(stdt) + '\n')
 	file_trans.write(str(x) + ',' + str(np.mean(trans_super)) + ',' + str(np.std(trans_super)) + ',' + str(np.mean(trans_ord)) + ',' + str(np.std(trans_ord)) + ',' + str(np.mean(trans_weak)) + ',' + str(np.std(trans_weak)) + '\n')
+	file_ginny.write(str(x) + ',' + str(np.mean(ginny)) + ',' + str(np.std(ginny)) + '\n')
 
 
 file_percent.close()
 file_time.close()	
 file_trans.close()
+file_ginny.close()
 # done 2b
-
+"""
 # varying s
-file_percent = open("s_percent.csv",'w')
-file_time = open("s_time.csv",'w')
-file_trans = open("s_trans.csv",'w')
+file_percent = open("part2/s_percent.csv",'w')
+file_time = open("part2/s_time.csv",'w')
+file_trans = open("part2/s_trans.csv",'w')
 
 z = subprocess.check_output(["python", "algo2a.py", str(1), str(70), str(50), str(50), str(26) ])
 y = z.split()
@@ -122,9 +145,9 @@ file_trans.close()
 # done 2c
 
 # varying l
-file_percent = open("l_percent.csv",'w')
-file_time = open("l_time.csv",'w')
-file_trans = open("l_trans.csv",'w')
+file_percent = open("part2/l_percent.csv",'w')
+file_time = open("part2/l_time.csv",'w')
+file_trans = open("part2/l_trans.csv",'w')
 
 for l in range(10,100,10):		# set range(100)
 	z = subprocess.check_output(["python", "algo2a.py", str(0.5), str(l), str(50), str(50), str(26) ])
@@ -140,3 +163,4 @@ file_percent.close()
 file_time.close()
 file_trans.close()
 # done 2d
+"""
