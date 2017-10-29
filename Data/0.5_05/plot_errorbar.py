2dbe2eb7e224d3aa119619163ab8f1980fbf521f
ankesh007 <ankeshgupta007@gmail.com>
2017-10-29 16:22:26 +0530

    Updated plotter

--- Data/0.5_05/plot_errorbar.py
+++ Data/0.5_05/plot_errorbar.py
@@ -3,17 +3,20 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 
-csv_path='avg_percent.csv'
+
+csv_name='avg_time'
+csv_path=csv_name+'.csv'
 
 ds=pd.read_csv(csv_path)
 np_arr=ds.values
 
 print np_arr.shape
+np_arr=np_arr[0:67,:]
 
 plt.figure()
 plt.errorbar(np_arr[:,0],np_arr[:,1],yerr=np_arr[:,2],ecolor='red')
-plt.title("Average")
+plt.title("Average Percentage of Nodes Reached v/s\n Transmission Probability to SuperNodes for S = 0.5% and L = 5%")
 plt.xlabel("Transmission Probability to SuperNodes(X) in %")
 plt.ylabel("Average % of Nodes Reached")
-plt.savefig('avg_percent'+'.png')
+plt.savefig(csv_name+'.png',bbox_inches='tight')
 # plt.show()
